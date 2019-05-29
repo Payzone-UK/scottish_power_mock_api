ff26944bcd670ca01f37591c726f2d82c45ba9bb
Guy Roberts <groberts2001@hotmail.com>
2019-05-29 13:19:50 +0100

    Full 253/254

--- src/services/easy_soap.ts
+++ src/services/easy_soap.ts
@@ -71,30 +71,6 @@ export class EasySoap {
             "</soapenv:Envelope>");
         return(retVal);
     }
-
-    get254XML(externalId: string, paymentIdentifier: string, utrn: string, error: any): string {
-        let xmlForError: String = "";
-
-        if (error) {
-            xmlForError = " <Message>" +
-                "             <Type>" + error[0] + "</Type>" +
-                "             <Code>" + error[1] + "</Code>" +
-                "             <Text>" + error[2] + "</Text>" +
-                "           </Message>";
-        }
-
-        const retVal = (
-            "         <Response>" +
-            "            <ExternalID>" + externalId + "</ExternalID>" +
-            "            <PaymentIdentifier>" + paymentIdentifier + "</PaymentIdentifier>" +
-            "            <UTRN>" + utrn + "</UTRN>" +
-            "         </Response>" +
-            "         <Messages>" +
-                        xmlForError +
-            "         </Messages>");
-
-        return(retVal);
-    }
 }
 
 /*
@@ -116,40 +92,5 @@ export class EasySoap {
 	       </Messages>
       </ED_PrepaymentResponseOutput_252>
    </soapenv:Body>
-</soapenv:Envelope>
- */
-
-/*
- This is the response for a reversal
-
-<?xml version="1.0" encoding="UTF-8"?>
-<ns0:MT_PrepaymentReversalResponseOutput_253_254 xmlns:ns0="urn:scottishpower.com:SMART:PAYOUTLET:ISU1:PrepaymentReversalSyncRequest:253_254">
-   <Response>
-      <ExternalID>MDCXO9QF2K1001X0800MJKL</ExternalID>
-      <PaymentIdentifier>6541256611653559</PaymentIdentifier>
-      <UTRN>85425635214513013369</UTRN>
-      <Messages>
-         <Message>
-            <Type>S</Type>
-            <Code>SAP-REVACC</Code>
-            <Text>Reversal Request Accepted</Text>
-         </Message>
-      </Messages>
-   </Response>
-</ns0:MT_PrepaymentReversalResponseOutput_253_254>
-
-The request XML below worked from curl
-
-<soapenv:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/'>
-    <soapenv:Header/>
-    <soapenv:Body>
-        <ns0:MT_PrepaymentReversalRequestInput_253_254 xmlns:ns0="urn:scottishpower.com:SMART:PAYOUTLET:ISU1:PrepaymentReversalSyncRequest:253_254">
-           <Request>
-              <ExternalID>016026180710</ExternalID>
-              <PaymentIdentifier>6332269160261807100</PaymentIdentifier>
-              <UTRN>12345685245469513574</UTRN>
-           </Request>
-        </ns0:MT_PrepaymentReversalRequestInput_253_254>
-    </soapenv:Body>
 </soapenv:Envelope>
  */
